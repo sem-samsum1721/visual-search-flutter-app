17981d720792ec7fd0c80683fc362ea97a7af56c
sem_samsum <128489573+sem-samsum1721@users.noreply.github.com>
2025-08-14 00:11:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const ProductSearchScreen(), // Burada yeni ekranınızı çağırın
+      home: const ProductSearchScreen(), 
     );
   }
 }
